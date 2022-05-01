d28817d20db7276610200ab30a2c1e27e36a5c27
Pratyay Gaurav <77407830+pratyaygaurav@users.noreply.github.com>
2022-05-01 16:30:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const App = () => {
+
+    let time = new Date().toLocaleTimeString();
+
+    const [ctime,setCtime] = useState(time);
+
+    const UpdateTime = () => {
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+    }
+
+    setInterval(UpdateTime,1000)
+
+
+    return(
+        <>  
+        <div className="content">
+            <h1>{ctime}</h1>
+            
+            </div>
+        </>
+
+    );
+}
+
+export default App;
